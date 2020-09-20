21fe5a71cd04466b9ccc84510629cc725c9e2857
Surkhojb <juanjo_berenguer@hotmail.com>
2020-09-20 22:33:31 +0200

    Created dialog to insert items on the db; Created swipe to delete item; Refactor snackbar

---
+++ app/src/main/java/com/surkhojb/groceryapp/feature/common/SwipeCallBack.kt
@@ -0,0 +1,28 @@
+package com.surkhojb.groceryapp.feature.common
+
+import android.content.Context
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+
+
+abstract class SwipeCallBack(var context: Context): ItemTouchHelper.SimpleCallback(ItemTouchHelper.LEFT,ItemTouchHelper.LEFT) {
+
+    override fun getMovementFlags(
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder
+    ): Int {
+        return ItemTouchHelper.Callback.makeMovementFlags(
+            0,
+            ItemTouchHelper.LEFT
+        )
+    }
+
+    override fun onMove(
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        viewHolder1: RecyclerView.ViewHolder
+    ): Boolean {
+        return false
+    }
+
+}
